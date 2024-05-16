25710222a0a9c62e1514b4c6a088c0cb2fc116a4
AJ Lera <you@example.com>
2024-05-16 15:09:30 +0800

    7th push

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,35 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import useFetch from "../hook/useFetch";
 
 const Home = () => {
   const API = `${process.env.REACT_APP_API_URL}/api/`;
   const URL = `${API}user/current-user`;
-  const { data, loading, error, refresh } = useFetch(`${API}users`);
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
   useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(URL);
+        if (!response.ok) {
+          setLoading(false);
+          setError("Network response was not ok");
+          console.error(response);
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setData(data);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        console.error(error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
     console.log(data);
     console.error(error);
   }, []);
