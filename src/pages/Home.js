33e574c94f5d2eaf724c56ce0550ddcc59d8580e
AJ Lera <you@example.com>
2024-05-17 08:50:58 +0800

    y

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,52 +3,36 @@ import Navbar from "../components/Navbar";
 import useFetch from "../hook/useFetch";
 
 const Home = () => {
-  const API = `${process.env.REACT_APP_API_URL}/api/`;
-  const URL = `${API}user/current-user`;
+  const API = `${process.env.REACT_APP_API_URL}`;
+  const URL = `${API}/api/users`;
   const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const response = await fetch(URL);
-        if (!response.ok) {
-          setLoading(false);
-          setError("Network response was not ok");
-          console.error(response);
-          throw new Error("Network response was not ok");
-        }
-        const data = await response.json();
-        setData(data);
-        setLoading(false);
-      } catch (error) {
-        setError(error);
-        console.error(error);
-        setLoading(false);
+  const fetchData = async () => {
+    try {
+      const response = await fetch(URL);
+      if (!response.ok) {
+        console.error(response);
+        throw new Error("Network response was not ok");
       }
-    };
-
+      const data = await response.json();
+      setData(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
     fetchData();
-    console.log(data);
-    console.error(error);
   }, []);
 
   return (
     <div>
       <Navbar />
       <section>Home</section>
-      {loading ? (
-        <p>Loading...</p>
-      ) : data ? (
-        <ul>
-          {data.map((user) => (
-            <li key={user.id}>{user.fullName}</li>
-          ))}
-        </ul>
-      ) : (
-        <p>No data</p>
-      )}
+
+      <ul>
+        {data?.map((user) => (
+          <li key={user.id}>{user.fullName}</li>
+        ))}
+      </ul>
     </div>
   );
 };
